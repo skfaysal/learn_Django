c8970bbd120f677051ce3ad9b1666e269a1c932f
skfaysal <faysal@smart-retina.com>
2021-02-26 01:52:09 +0600

    Add two number

--- testProject/calc/urls.py
+++ testProject/calc/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('',views.home, name = 'home') # for getting the home page you have to go home function in views module
+	path('',views.home, name = 'home'), # 1st of all it will show the home.html content
+	path('add',views.add,name = 'add') # if add button is pressed it will direct into this path and call add function from views.py
 ]
--- testProject/calc/views.py
+++ testProject/calc/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html',{'name':'Faysal'})  # get request from the user and return http request
+
+
+def add(request):
+    val1=int(request.GET['num1'])
+    val2=int(request.GET['num2'])
+
+    res= val1+val2
+
+    return render(request,'results.html',{'result':res})
+
