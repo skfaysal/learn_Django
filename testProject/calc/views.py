6e2b3d3d79f1c731d0c86fa715f1711810ea7921
skfaysal <faysal@smart-retina.com>
2021-02-26 00:47:22 +0600

    DTL dynamic fetch name from views

--- testProject/calc/views.py
+++ testProject/calc/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
-	return render(request,'home.html') # get request from the user and return http request
+    return render(request, 'home.html',{'name':'Faysal'})  # get request from the user and return http request
