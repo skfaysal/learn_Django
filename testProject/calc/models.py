8b79a86728180712d157708663354b0abbd9bab6
skfaysal <faysal@smart-retina.com>
2021-02-27 17:16:47 +0600

    Create DB and add data from shell

--- testProject/calc/models.py
+++ testProject/calc/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Diseases(models.Model):
+    num1=models.CharField(max_length=100)
+    num2=models.CharField(max_length=100)
+
+
--- testProject/calc/views.py
+++ testProject/calc/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Diseases
 
 # Create your views here.
 def home(request):
@@ -8,10 +8,26 @@ def home(request):
 
 
 def add(request):
-    val1=int(request.GET['num1'])
-    val2=int(request.GET['num2'])
 
-    res= val1+val2
+    if request.method == 'POST':
+        num1=request.POST['num1']
+        num2=request.POST['num2']
+        print(num1,num2)
+        obj=Diseases()
+        obj.num1=num1
+        obj.num2=num2
+        obj.save
 
-    return render(request,'results.html',{'result':res})
+    context={
+
+    }
+
+
+
+    # val1=int(request.GET['num1'])
+    # val2=int(request.GET['num2'])
+
+    # res= val1+val2
+
+    return render(request,'home.html',context)
 
