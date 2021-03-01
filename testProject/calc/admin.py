fc4accd449f38ac6e1e7d57bcb3e75ac64b1fa05
skfaysal <faysal@smart-retina.com>
2021-03-01 15:40:11 +0600

    Create DB with many to one relation

--- testProject/calc/admin.py
+++ testProject/calc/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Diseases
-# Register your models here.
+from .models import Customer
+from .models import Tag
+from .models import Product
+from .models import Order
+
 admin.site.register(Diseases)
+admin.site.register(Customer)
+admin.site.register(Tag)
+admin.site.register(Product)
+admin.site.register(Order)
